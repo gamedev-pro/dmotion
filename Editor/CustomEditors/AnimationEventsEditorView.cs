cc20e40f126b6b52938f30f13e3d689cac607305
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-07-11 15:08:17 -0700

    Build Animation Events view

---
+++ Editor/CustomEditors/AnimationEventsEditorView.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DOTSAnimation.Editor
+{
+    public class AnimationEventsEditorView : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<AnimationEventsEditorView, UxmlTraits>
+        {
+            
+        }
+    }
+}
