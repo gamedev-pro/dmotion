7b62f83310eac88ab2175a633fca14beede6d14d
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-07-14 17:40:08 -0700

    Add event button

--- Editor/CustomEditors/AnimationClipAssetEditor.cs
+++ Editor/CustomEditors/AnimationClipAssetEditor.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using DOTSAnimation.Authoring;
 using UnityEditor;
 using UnityEngine;
--- Editor/CustomEditors/AnimationEventsEditorView.cs
+++ Editor/CustomEditors/AnimationEventsEditorView.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using DOTSAnimation.Authoring;
 using UnityEditor;
 using UnityEngine;
 
 namespace DOTSAnimation.Editor
 {
-    public struct DragRect
+    public struct RectElement
     {
         public Rect Rect;
         public float ControlWidth;
         public float VisualWidth;
 
-        public DragRect(Rect rect, float controlWidth, float visualWidth)
+        public RectElement(Rect rect, float controlWidth, float visualWidth)
         {
             Rect = rect;
             ControlWidth = controlWidth;
@@ -41,11 +42,14 @@ namespace DOTSAnimation.Editor
     {
         private static readonly Texture2D whiteTex;
         private static Texture2D eventMarkerTex;
+        private static GUIStyle addRemoveEventStyle;
+        private static GUIContent addEventContent;
+        private static GUIContent removeEventContent;
 
         private Rect dragArea;
-        private DragRect timeMarker;
+        private RectElement timeMarker;
         private float timeMarkerTime;
-        private List<DragRect> eventMarkers = new();
+        private List<RectElement> eventMarkers = new();
         private bool isDraggingTimeMarker = false;
         private int eventMarkerDragIndex = -1;
         private readonly SingleClipPreview preview;
@@ -55,14 +59,54 @@ namespace DOTSAnimation.Editor
         {
             get
             {
-                if (eventMarkerTex != null)
+                if (eventMarkerTex == null)
                 {
-                    return eventMarkerTex;
+                    eventMarkerTex = (Texture2D) EditorGUIUtility.Load("Animation.EventMarker");
+                    eventMarkerTex.filterMode = FilterMode.Bilinear;
+                }
+                return eventMarkerTex;
+            }
+        }
+
+        private static GUIStyle AddRemoveEventStyle
+        {
+            get
+            {
+                if (addRemoveEventStyle == null)
+                {
+                    addRemoveEventStyle = new GUIStyle(EditorStyles.miniButton)
+                    {
+                        fixedHeight = 0,
+                        padding = new RectOffset(-1, 1, 0, 0)
+                    };
                 }
 
-                eventMarkerTex = (Texture2D) EditorGUIUtility.Load("Animation.EventMarker");
-                eventMarkerTex.filterMode = FilterMode.Bilinear;
-                return eventMarkerTex;
+                return addRemoveEventStyle;
+            }
+        }
+
+        private static GUIContent RemoveEventContent
+        {
+            get
+            {
+                if (removeEventContent == null)
+                {
+                    removeEventContent = new GUIContent("X");
+                }
+                return removeEventContent;
+            }
+        }
+
+        private static GUIContent AddEventContent
+        {
+            get
+            {
+                if (addEventContent == null)
+                {
+                    addEventContent = EditorGUIUtility.IconContent("Animation.AddEvent", "Add event");
+                }
+
+                return addEventContent;
             }
         }
 
@@ -88,14 +132,32 @@ namespace DOTSAnimation.Editor
             {
                 EditorGUI.LabelField(area, label);
 
+                var addEventButtonWidth = area.height;//square button
                 dragArea = area;
                 dragArea.x += 60;
+                dragArea.xMax -= (addEventButtonWidth + EditorGUIUtility.standardVerticalSpacing);
                 DrawEventsDragArea(dragArea);
+
+                var addEventButtonRect = area;
+                addEventButtonRect.width = addEventButtonWidth;
+                addEventButtonRect.x = dragArea.xMax + EditorGUIUtility.standardVerticalSpacing;
+                DrawAddRemoveEventButton(addEventButtonRect);
             }
             GUI.color = Color.white;
             EditorGUILayout.PropertyField(property, GUIContent.none, true);
             
             HandleEvents();
+        }
+
+        private void DrawAddRemoveEventButton(Rect addEventButtonRect)
+        {
+            if (GUI.Button(addEventButtonRect, AddEventContent, AddRemoveEventStyle))
+            {
+                clipAsset.Events = clipAsset.Events.Append(new AnimationClipEvent()
+                {
+                    NormalizedTime = timeMarkerTime
+                }).ToArray();
+            }
         }
 
         private void HandleEvents()
@@ -227,7 +289,7 @@ namespace DOTSAnimation.Editor
                 GUI.DrawTexture(bottomBarRect, whiteTex);
             }
             {
-                timeMarker = new DragRect(area, 10, 2);
+                timeMarker = new RectElement(area, 10, 2);
                 timeMarker.Rect.x = NormalizedTimeToPixels(timeMarkerTime, timeMarker.VisualRect, area);
                 GUI.color = Color.red;
                 GUI.DrawTexture(timeMarker.VisualRect, whiteTex);
@@ -237,7 +299,7 @@ namespace DOTSAnimation.Editor
                 for (var i = 0; i < clipAsset.Events.Length; i++)
                 {
                     var e = clipAsset.Events[i];
-                    var eventMarkerRect = new DragRect(area, 10, 5);
+                    var eventMarkerRect = new RectElement(area, 10, 5);
                     eventMarkerRect.Rect.height *= 0.9f;
                     eventMarkerRect.Rect.x = NormalizedTimeToPixels(e.NormalizedTime, eventMarkerRect.VisualRect, area);
                     GUI.color = Color.white;
