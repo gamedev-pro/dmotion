6b083b75eb13c6864f66ea95aa192ea20bf20956
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-07-19 20:40:22 -0700

    Use active samplers on RaiseEvents

--- Runtime/Components/AnimationState.cs
+++ Runtime/Components/AnimationState.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Runtime.CompilerServices;
-using BovineLabs.Event.Containers;
 using Latios.Kinemation;
 using Unity.Burst;
 using Unity.Entities;
-using UnityEngine;
 
 namespace DOTSAnimation
 {
@@ -12,6 +9,7 @@ namespace DOTSAnimation
     {
         internal BlobAssetReference<SkeletonClipSetBlob> Clips;
         internal ushort ClipIndex;
+        internal float PreviousNormalizedTime;
         internal float NormalizedTime;
         internal float Weight;
 
@@ -40,11 +38,12 @@ namespace DOTSAnimation
 
         internal void UpdateSamplers(float dt, float blendWeight, ref DynamicBuffer<ClipSampler> samplers, ref ActiveSamplersCount activeSamplersCount)
         {
+            var prevNormalizedTime = NormalizedTime;
             NormalizedTime += dt * Speed;
             switch (Type)
             {
                 case StateType.Single:
-                    UpdateSamplers_Single(blendWeight, ref samplers, ref activeSamplersCount);
+                    UpdateSamplers_Single(prevNormalizedTime, blendWeight, ref samplers, ref activeSamplersCount);
                     break;
                 case StateType.LinearBlend:
                 default:
@@ -52,64 +51,19 @@ namespace DOTSAnimation
             }
         }
 
-        private readonly void UpdateSamplers_Single(float blendWeight, ref DynamicBuffer<ClipSampler> samplers, ref ActiveSamplersCount activeSamplersCount)
+        private readonly void UpdateSamplers_Single(float prevNormalizedTime, float blendWeight, ref DynamicBuffer<ClipSampler> samplers, ref ActiveSamplersCount activeSamplersCount)
         {
             var samplerIndex = activeSamplersCount.Value;
             
             var sampler = samplers[samplerIndex];
             sampler.Clips = Clips;
             sampler.ClipIndex = AsSingleClip.ClipIndex;
+            sampler.PreviousNormalizedTime = Loop ? sampler.Clip.LoopToClipTime(prevNormalizedTime) : prevNormalizedTime;
             sampler.NormalizedTime = Loop ? sampler.Clip.LoopToClipTime(NormalizedTime) : NormalizedTime;
             sampler.Weight = blendWeight;
             samplers[samplerIndex] = sampler;
             
             activeSamplersCount.Value++;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal readonly float GetNormalizedTimeShifted(float dt)
-        {
-            return NormalizedTime - dt * Speed;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal readonly void RaiseStateEvents(float dt, Entity animatorEntity, Entity ownerEntity, ref NativeEventStream.ThreadWriter Writer)
-        {
-            switch (Type)
-            {
-                case StateType.Single:
-                    ref var singleClipState = ref AsSingleClip;
-                    var currentNormalizedTime = NormalizedTime;
-                    var previousNormalizedTime = GetNormalizedTimeShifted(dt);
-                    
-                    if (StateBlob.Loop)
-                    {
-                        ref var clip = ref Clips.Value.clips[singleClipState.ClipIndex];
-                        currentNormalizedTime = clip.LoopToClipTime(currentNormalizedTime);
-                        previousNormalizedTime = clip.LoopToClipTime(previousNormalizedTime);
-                    }
-
-                    ref var clipEvents = ref StateMachineBlob.Value.ClipEvents;
-                    for (short i = 0; i < clipEvents.Length; i++)
-                    {
-                        ref var e = ref clipEvents[i];
-                        if (e.ClipIndex == singleClipState.ClipIndex &&
-                            e.NormalizedTime >= previousNormalizedTime && e.NormalizedTime <= currentNormalizedTime)
-                        {
-                            Debug.Log("YO");
-                            Writer.Write(new RaisedAnimationEvent()
-                            {
-                                EventHash = e.EventHash,
-                                AnimatorEntity = animatorEntity,
-                                AnimatorOwner = ownerEntity,
-                            });
-                        }
-                    }
-                    break;
-                case StateType.LinearBlend:
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
         }
     }
 }
--- Runtime/Systems/RaiseAnimationEventsJob.cs
+++ Runtime/Systems/RaiseAnimationEventsJob.cs
@@ -1,6 +1,7 @@
 using BovineLabs.Event.Containers;
 using Unity.Burst;
 using Unity.Entities;
+using UnityEngine;
 
 namespace DOTSAnimation
 {
@@ -12,14 +13,34 @@ namespace DOTSAnimation
         internal void Execute(
             Entity animatorEntity,
             in AnimationStateMachine stateMachine,
+            in DynamicBuffer<ClipSampler> samplers,
+            in ActiveSamplersCount activeSamplersCount,
             in AnimatorEntity animatorOwner
         )
         {
-            //Raise events
-            stateMachine.CurrentState.RaiseStateEvents(DeltaTime, animatorEntity, animatorOwner.Owner, ref Writer);
-            if (stateMachine.CurrentTransition.IsValid)
+            //TODO: Events should not be tied to a state machine, but to a separate blob instead (otherwise one shots can't raise events)
+            ref var clipEvents = ref stateMachine.StateMachineBlob.Value.ClipEvents;
+            for (byte samplerIndex = 0; samplerIndex < activeSamplersCount.Value; samplerIndex++)
             {
-                stateMachine.NextState.RaiseStateEvents(DeltaTime, animatorEntity, animatorOwner.Owner, ref Writer);
+                var sampler = samplers[samplerIndex];
+                var clipIndex = sampler.ClipIndex;
+                var previousSamplerTime = sampler.PreviousNormalizedTime;
+                var currentSamplerTime = sampler.NormalizedTime;
+                for (short i = 0; i < clipEvents.Length; i++)
+                {
+                    ref var e = ref clipEvents[i];
+                    if (e.ClipIndex == clipIndex &&
+                        e.NormalizedTime >= previousSamplerTime && e.NormalizedTime <= currentSamplerTime)
+                    {
+                        Debug.Log("YO");
+                        Writer.Write(new RaisedAnimationEvent()
+                        {
+                            EventHash = e.EventHash,
+                            AnimatorEntity = animatorEntity,
+                            AnimatorOwner = animatorOwner.Owner
+                        });
+                    }
+                }
             }
         }
     }
