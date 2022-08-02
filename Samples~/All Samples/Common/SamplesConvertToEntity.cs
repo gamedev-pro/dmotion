9d4f28a952898cd557578d9b3ae18714ddda0227
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-08-01 19:49:23 -0700

    Samples support Netcode

---
+++ Samples~/All Samples/Common/SamplesConvertToEntity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DMotion.Samples
+{
+    public class SamplesConvertToEntity : MonoBehaviour
+    {
+        private void Awake()
+        {
+            #if NETCODE_PROJECT
+            gameObject.AddComponent<Unity.NetCode.ConvertToClientServerEntity>();
+            #else
+            gameObject.AddComponent<Unity.Entities.ConvertToEntity>();
+            #endif
+        }
+    }
+}
