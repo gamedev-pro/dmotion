87358743e74024cdc424ce056a0caa1e2387562b
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-08-13 18:58:30 -0700

    Fix build

--- Runtime/Components/AnimationParameters.cs
+++ Runtime/Components/AnimationParameters.cs
@@ -10,6 +10,14 @@ namespace DMotion
         #endif
         public int Hash;
         public bool Value;
+        public BoolParameter(FixedString64Bytes name, int hash)
+        {
+#if UNITY_EDITOR || DEBUG
+            Name = name;
+#endif
+            Hash = hash;
+            Value = false;
+        }
     }
     
     public struct BlendParameter : IBufferElementData
@@ -19,5 +27,14 @@ namespace DMotion
         #endif
         public int Hash;
         public float Value;
+        
+        public BlendParameter(FixedString64Bytes name, int hash)
+        {
+#if UNITY_EDITOR || DEBUG
+            Name = name;
+#endif
+            Hash = hash;
+            Value = 0;
+        }
     }
 }
