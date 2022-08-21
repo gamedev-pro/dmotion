7e417b3a2b8d72ff8a217094125ad41244b19abc
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-08-20 22:18:04 -0700

    Create minimum prefab for state machine tests

---
+++ Tests/Editor/StateMachineTestUtils.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+
+namespace DMotion.Tests
+{
+    public static class StateMachineTestUtils
+    {
+        public static Entity InstantiateStateMachineEntity(this EntityManager manager, Entity prefab)
+        {
+            var newEntity = manager.Instantiate(prefab);
+            Assert.IsTrue(manager.HasComponent<AnimationStateMachine>(newEntity));
+            return newEntity;
+        }
+
+        public static void SetBoolParameter(this EntityManager manager, Entity entity, int index, bool newValue)
+        {
+            Assert.IsTrue(manager.HasComponent<BoolParameter>(entity));
+            var boolParameters = manager.GetBuffer<BoolParameter>(entity);
+            Assert.IsTrue(boolParameters.Length > 0);
+            var parameter = boolParameters[index];
+            parameter.Value = newValue;
+            boolParameters[index] = parameter;
+        }
+    }
+}
--- Tests/Editor/UpdateStateMachineJobShould.cs
+++ Tests/Editor/UpdateStateMachineJobShould.cs
@@ -6,26 +6,6 @@ using UnityEngine;
 
 namespace DMotion.Tests
 {
-    public static class StateMachineTestUtils
-    {
-        public static Entity InstantiateStateMachineEntity(this EntityManager manager, Entity prefab)
-        {
-            var newEntity = manager.Instantiate(prefab);
-            Assert.IsTrue(manager.HasComponent<AnimationStateMachine>(newEntity));
-            return newEntity;
-        }
-
-        public static void SetBoolParameter(this EntityManager manager, Entity entity, int index, bool newValue)
-        {
-            Assert.IsTrue(manager.HasComponent<BoolParameter>(entity));
-            var boolParameters = manager.GetBuffer<BoolParameter>(entity);
-            Assert.IsTrue(boolParameters.Length > 0);
-            var parameter = boolParameters[index];
-            parameter.Value = newValue;
-            boolParameters[index] = parameter;
-        }
-    }
-    
     [CreateSystemsForTest(typeof(AnimationStateMachineSystem))]
     public class UpdateStateMachineJobShould : ECSTestsFixture
     {
