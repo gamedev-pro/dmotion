37ac573af89392cd1b0ee2888498ce11715e3c89
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-09-10 15:55:14 -0700

    Add performance benchmarks

--- Tests/Editor/Core/PerformanceTestUtils.cs
+++ Tests/Editor/Core/PerformanceTestUtils.cs
@@ -1,9 +1,43 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using NUnit.Framework;
+using Unity.PerformanceTesting;
+using Unity.PerformanceTesting.Runtime;
 using Unity.Profiling;
+using UnityEngine;
 
 namespace DMotion.Tests
 {
+    [Serializable]
+    public struct TestBenchmarkValue
+    {
+        [Tooltip("Expected value")] public double Value;
+
+        [UnityEngine.Range(1, 7), Tooltip("Acceptable tolerance +/-")]
+        public int Tolerance;
+
+        public static TestBenchmarkValue DefaultTolerance => new TestBenchmarkValue() { Tolerance = 10 };
+
+        public double Min => Value * (1 - Tolerance / 100f);
+        public double Max => Value * (1 + Tolerance / 100f);
+    }
+
+    [Serializable]
+    public struct PerformanceTestBenchmark
+    {
+        public int Count;
+        public TestBenchmarkValue Median;
+        public TestBenchmarkValue Max;
+        public TestBenchmarkValue Min;
+    }
+
+    [Serializable]
+    public struct MachineSpecificPerformanceBenchmarks
+    {
+        public string MachineName;
+        public PerformanceTestBenchmark[] Benchmarks;
+    }
+
     public static class PerformanceTestUtils
     {
         public static string GetNameSlow(this ProfilerMarker marker)
@@ -16,6 +50,23 @@ namespace DMotion.Tests
             Assert.NotNull(name);
             Assert.IsNotEmpty(name);
             return name;
+        }
+
+        public static void AssertWithinBenchmark(this PerformanceTestBenchmark benchmark)
+        {
+            static void AssertTestResult(string propertyName, in TestBenchmarkValue benchmarkValue, double actual)
+            {
+                Assert.NotZero(actual, "Received 0 for actual values. This is probably no correct");
+                Assert.GreaterOrEqual(actual, benchmarkValue.Min, $"{propertyName}: Actual {actual} below Min value {benchmarkValue.Min} (Expected: {benchmarkValue.Value}, Tolerance: {benchmarkValue.Tolerance}%. Did performance improve?");
+                Assert.LessOrEqual(actual, benchmarkValue.Max, $"{propertyName}: Actual {actual} above Max acceptable value {benchmarkValue.Max} (Expected: {benchmarkValue.Value}, Tolerance: {benchmarkValue.Tolerance}%. Did performance decrease?");
+            }
+            
+            Assert.Greater(PerformanceTest.Active.SampleGroups.Count, 0, "Not Sample groups found to benchmark against");
+            var sampleGroup = PerformanceTest.Active.SampleGroups[0];
+            sampleGroup.UpdateStatistics();
+            AssertTestResult(nameof(benchmark.Median), benchmark.Median, sampleGroup.Median);
+            AssertTestResult(nameof(benchmark.Min), benchmark.Min, sampleGroup.Min);
+            AssertTestResult(nameof(benchmark.Max), benchmark.Max, sampleGroup.Max);
         }
     }
 }
--- Tests/Editor/Performance/StateMachinePerformanceTests.cs
+++ Tests/Editor/Performance/StateMachinePerformanceTests.cs
@@ -1,4 +1,5 @@
-﻿using DMotion.Tests;
+﻿using System;
+using DMotion.Tests;
 using NUnit.Framework;
 using Unity.Entities;
 using Unity.PerformanceTesting;
@@ -11,44 +12,75 @@ namespace DMotion.PerformanceTests
     {
         [SerializeField, ConvertGameObjectPrefab(nameof(noSkeletonPrefabEntity))]
         private GameObject noSkeletonPrefab;
-        
+
         [SerializeField, ConvertGameObjectPrefab(nameof(skeletonPrefabEntity))]
         private GameObject skeletonPrefab;
 
+        [SerializeField] private PerformanceTestBenchmarksPerMachine avgUpdateTimeBenchmarks;
+
         private Entity noSkeletonPrefabEntity;
         private Entity skeletonPrefabEntity;
-        
-        static int[] testValues = { 1000, 10_000, 100_000 };
-        
+
+        private static int[] testValues = { 1000, 10_000, 100_000 };
+
         [Test, Performance]
         public void AverageUpdateTime([ValueSource(nameof(testValues))] int count)
         {
             InstantiateEntities(count, skeletonPrefabEntity);
             Measure.Method(() => UpdateWorld())
-                .WarmupCount(2)
-                .MeasurementCount(10)
+                .WarmupCount(5)
+                .MeasurementCount(20)
                 .IterationsPerMeasurement(1)
                 .Run();
+
+            if (TryGetBenchmarkForCount(count, avgUpdateTimeBenchmarks, out var benchmark))
+            {
+                benchmark.AssertWithinBenchmark();
+            }
         }
-        
-        [Test, Performance]
-        public void JobMarkers([ValueSource(nameof(testValues))] int count)
+
+        private bool TryGetBenchmarkForCount(int count, PerformanceTestBenchmarksPerMachine groupsAsset,
+            out PerformanceTestBenchmark benchmark)
         {
-            InstantiateEntities(count, skeletonPrefabEntity);
-            using var markersMeasurement = Measure.ProfilerMarkers(
-                    AnimationStateMachineSystem.Marker_UpdateStateMachineJob.GetNameSlow(),
-                    AnimationStateMachineSystem.Marker_SampleOptimizedBonesJob.GetNameSlow(),
-                    UpdateStateMachines.Marker.GetNameSlow()
-                );
-            UpdateWorld();
+            var groups = groupsAsset.MachineBenchmarks;
+            benchmark = default;
+            if (groups == null || groups.Length == 0)
+            {
+                return false;
+            }
+
+            //TODO: chose for machine
+            var group = groups[0];
+            
+            Assert.IsNotNull(group.Benchmarks);
+            var index = Array.FindIndex(group.Benchmarks, b => b.Count == count);
+            if (index < 0)
+            {
+                return false;
+            }
+
+            benchmark = group.Benchmarks[index];
+            return true;
         }
+
+        // [Test, Performance]
+        // public void JobMarkers([ValueSource(nameof(testValues))] int count)
+        // {
+        //     InstantiateEntities(count, skeletonPrefabEntity);
+        //     using var markersMeasurement = Measure.ProfilerMarkers(
+        //             AnimationStateMachineSystem.Marker_UpdateStateMachineJob.GetNameSlow(),
+        //             AnimationStateMachineSystem.Marker_SampleOptimizedBonesJob.GetNameSlow(),
+        //             UpdateStateMachines.Marker.GetNameSlow()
+        //         );
+        //     UpdateWorld();
+        // }
 
         private void InstantiateEntities(int count, Entity prefab)
         {
             for (var i = 0; i < count; i++)
             {
                 var e = manager.Instantiate(prefab);
-                
+
                 Assert.IsTrue(manager.HasComponent<LinearBlendDirection>(e));
                 Assert.IsTrue(manager.HasComponent<PlayOneShotRequest>(e));
                 Assert.IsTrue(manager.HasComponent<BlendParameter>(e));
