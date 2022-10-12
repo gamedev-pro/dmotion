ba4438e0a73f79038bcb25491794ea7b2c0d08c7
Gabriel Dechichi <gabrieldechichi@gmail.com>
2022-10-11 21:29:09 -0700

    Generalize Parameter setting

--- Runtime/Components/AnimationParameters.cs
+++ Runtime/Components/AnimationParameters.cs
@@ -1,39 +1,63 @@
-﻿using Unity.Collections;
+﻿using System.Runtime.CompilerServices;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace DMotion
 {
     public interface IHasHash
     {
-        public int Hash { get; }
+        int Hash { get; }
     }
-    public struct IntParameter : IBufferElementData, IHasHash
+
+    public interface IStateMachineParameter<T> : IHasHash
+        where T : struct
     {
-         #if UNITY_EDITOR || DEBUG
-         public FixedString64Bytes Name;
-         #endif
-         public int Hash;
-         public int Value;
-        int IHasHash.Hash => Hash;
-         public IntParameter(FixedString64Bytes name, int hash)
-         {
- #if UNITY_EDITOR || DEBUG
-             Name = name;
- #endif
-             Hash = hash;
-             Value = 0;
-         }       
+        T Value { get; set; }
     }
-    
-    public struct BoolParameter : IBufferElementData, IHasHash
+
+    public struct IntParameter : IBufferElementData, IStateMachineParameter<int>
     {
-        #if UNITY_EDITOR || DEBUG
+#if UNITY_EDITOR || DEBUG
         public FixedString64Bytes Name;
-        #endif
+#endif
+        public int Hash;
+        public int Value;
+        int IHasHash.Hash => Hash;
+        int IStateMachineParameter<int>.Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Value;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => Value = value;
+        }
+
+        public IntParameter(FixedString64Bytes name, int hash)
+        {
+#if UNITY_EDITOR || DEBUG
+            Name = name;
+#endif
+            Hash = hash;
+            Value = 0;
+        }
+    }
+
+    public struct BoolParameter : IBufferElementData, IStateMachineParameter<bool>
+    {
+#if UNITY_EDITOR || DEBUG
+        public FixedString64Bytes Name;
+#endif
         public int Hash;
         public bool Value;
 
         int IHasHash.Hash => Hash;
+        bool IStateMachineParameter<bool>.Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Value;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => Value = value;
+        }
+
         public BoolParameter(FixedString64Bytes name, int hash)
         {
 #if UNITY_EDITOR || DEBUG
@@ -43,16 +67,23 @@ namespace DMotion
             Value = false;
         }
     }
-    
-    public struct BlendParameter : IBufferElementData, IHasHash
+
+    public struct BlendParameter : IBufferElementData, IStateMachineParameter<float>
     {
-        #if UNITY_EDITOR || DEBUG
+#if UNITY_EDITOR || DEBUG
         public FixedString64Bytes Name;
-        #endif
+#endif
         public int Hash;
         public float Value;
         int IHasHash.Hash => Hash;
-        
+        float IStateMachineParameter<float>.Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Value;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => Value = value;
+        }
+
         public BlendParameter(FixedString64Bytes name, int hash)
         {
 #if UNITY_EDITOR || DEBUG
--- Runtime/Utils/StateMachineParameterUtils.cs
+++ Runtime/Utils/StateMachineParameterUtils.cs
@@ -4,140 +4,6 @@ using Unity.Entities;
 
 namespace DMotion
 {
-    public static class BlendParameterUtils
-    {
-        public static void SetParameter(this DynamicBuffer<BlendParameter> parameters, int hash, float value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                var p = parameters[index];
-                p.Value = value;
-                parameters[index] = p;
-            }
-        }
-
-
-        public static void SetParameter(this DynamicBuffer<BlendParameter> parameters, FixedString32Bytes name,
-            float value)
-        {
-            var hash = name.GetHashCode();
-            parameters.SetParameter(hash, value);
-        }
-
-        public static void IncrementParameter(this DynamicBuffer<BlendParameter> parameters, int hash, float increment)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                var p = parameters[index];
-                p.Value += increment;
-                parameters[index] = p;
-            }
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<BlendParameter> parameters, int hash, out float value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                value = parameters[index].Value;
-                return true;
-            }
-
-            value = default;
-            return false;
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<BlendParameter> parameters, FixedString32Bytes name,
-            out float value)
-        {
-            var hash = name.GetHashCode();
-            return parameters.TryGetValue(hash, out value);
-        }
-    }
-
-    public static class BoolParameterUtils
-    {
-        public static void SetParameter(this DynamicBuffer<BoolParameter> parameters, int hash, bool value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                var p = parameters[index];
-                p.Value = value;
-                parameters[index] = p;
-            }
-        }
-
-
-        public static void SetParameter(this DynamicBuffer<BoolParameter> parameters, FixedString32Bytes name,
-            bool value)
-        {
-            var hash = name.GetHashCode();
-            parameters.SetParameter(hash, value);
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<BoolParameter> parameters, int hash, out bool value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                value = parameters[index].Value;
-                return true;
-            }
-
-            value = default;
-            return false;
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<BoolParameter> parameters, FixedString32Bytes name,
-            out bool value)
-        {
-            var hash = name.GetHashCode();
-            return parameters.TryGetValue(hash, out value);
-        }
-    }
-    public static class IntParameterUtils
-    {
-        public static void SetParameter(this DynamicBuffer<IntParameter> parameters, int hash, int value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                var p = parameters[index];
-                p.Value = value;
-                parameters[index] = p;
-            }
-        }
-
-        public static void SetParameter(this DynamicBuffer<IntParameter> parameters, FixedString32Bytes name, int value)
-        {
-            var hash = name.GetHashCode();
-            parameters.SetParameter(hash, value);
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<IntParameter> parameters, int hash, out int value)
-        {
-            var index = parameters.HashToIndex(hash);
-            if (index >= 0)
-            {
-                value = parameters[index].Value;
-                return true;
-            }
-
-            value = default;
-            return false;
-        }
-
-        public static bool TryGetValue(this DynamicBuffer<IntParameter> parameters, FixedString32Bytes name,
-            out int value)
-        {
-            var hash = name.GetHashCode();
-            return parameters.TryGetValue(hash, out value);
-        }
-    }
-
     [BurstCompile]
     public static class StateMachineParameterUtils
     {
@@ -158,6 +24,51 @@ namespace DMotion
         public static int GetHashCode(string name)
         {
             return name.GetHashCode();
+        }
+
+        public static void SetParameter<TBuffer, TValue>(this DynamicBuffer<TBuffer> parameters, int hash, TValue value)
+            where TBuffer : struct, IStateMachineParameter<TValue>
+            where TValue : struct
+        {
+            var index = parameters.HashToIndex(hash);
+            if (index >= 0)
+            {
+                var p = parameters[index];
+                p.Value = value;
+                parameters[index] = p;
+            }
+        }
+
+        public static void SetParameter<TBuffer, TValue>(this DynamicBuffer<TBuffer> parameters, FixedString32Bytes name, TValue value)
+            where TBuffer : struct, IStateMachineParameter<TValue>
+            where TValue : struct
+        {
+            var hash = name.GetHashCode();
+            parameters.SetParameter(hash, value);
+        }
+
+        public static bool TryGetValue<TBuffer, TValue>(this DynamicBuffer<TBuffer> parameters, int hash, out TValue value)
+            where TBuffer : struct, IStateMachineParameter<TValue>
+            where TValue : struct
+        {
+            var index = parameters.HashToIndex(hash);
+            if (index >= 0)
+            {
+                value = parameters[index].Value;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public static bool TryGetValue<TBuffer, TValue>(this DynamicBuffer<TBuffer> parameters, FixedString32Bytes name,
+            out TValue value)
+            where TBuffer : struct, IStateMachineParameter<TValue>
+            where TValue : struct
+        {
+            var hash = name.GetHashCode();
+            return parameters.TryGetValue(hash, out value);
         }
     }
 }
